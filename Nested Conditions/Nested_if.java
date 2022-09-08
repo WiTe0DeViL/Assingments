9460329d47318a56d95a6619808f42979f7dfdda
Man_Of_Darkness <72191625+WiTe0DeViL@users.noreply.github.com>
2022-09-08 10:13:33 +0530

    Nested IF

---
+++ Nested Conditions/Nested_if.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a = 10;
+	    String b = "Male";
+	    if(a >= 18){
+	        if(b.equals("Male")){
+	            System.out.println("Your Eligible to vote and go to Counter 1 for Male");
+	        }else{
+	            System.out.println("Your Counter is  Not here");
+	        }
+	    }else{
+	        System.out.println("Your are not eligible");
+	    }
+	    
+	}
+}
